3ca8bf5caf9a129dc49256c0bd3d09a30de45c04
Gus1331 <gustavooliver.souza@gmail.com>
2026-05-14 16:53:37 -0300

    fix: site deploy config especification

--- red-radiation/astro.config.mjs
+++ red-radiation/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://Gus1331.github.io",
+    site: "https://gus1331.github.io",
     base: "/Portfolio"
 });
